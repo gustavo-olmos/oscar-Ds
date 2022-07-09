12f76507a47fde60bca04652ad4054f376263670
Gustavo L O Ferraz <87102570+gustavo-olmos@users.noreply.github.com>
2022-07-09 18:45:42 -0300

    utils

---
+++ Utils.java
@@ -0,0 +1,28 @@
+package org.example.util;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.List;
+import java.util.function.Function;
+import java.util.stream.Collectors;
+
+public class Utils<T> {
+    private final String fileName;
+    public Utils(String fileName) {
+        this.fileName = fileName;
+    }
+
+    public List <T> leitorArquivo(Function<String, T> mapper) throws IOException {
+        var file = this.getClass().getClassLoader().getResourceAsStream(this.fileName);
+        if (file == null){
+            throw new IllegalArgumentException("Arquivo não encontrado");
+        }
+        try (BufferedReader reader = new BufferedReader(new InputStreamReader(file))){
+            return reader.lines()
+                    .skip(1)
+                    .map(mapper)
+                    .collect(Collectors.toList());
+        }
+    }
+}
